2837eb57a72007a59aac24d28c585cea53941da5
n33ti <52991838+n33ti@users.noreply.github.com>
2020-07-01 16:58:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,57 @@
+var library = [
+    {
+        title: "Javascript",
+        price: 700,
+        readers : [
+            {
+                name: "person1",
+                count : 300
+            },
+            {
+                name: "person2",
+                count : 300
+            }
+        ],
+        author : {
+            name : "Raj",
+            age: 40
+        }
+    },
+    {
+        title : "nodejs",
+        price : 300,
+        readers: [],
+        author: {
+            name: "Raj",
+            age: 40
+        }
+    }
+]
+console.log(library[0].readers[0])
+library[0].readers[0].count = 400;
+console.log(library[0].readers[0])
 
-var pull = require('lodash.pull');
-var difference = require('lodash.difference');
-var take = require('lodash.take');
-var filter = require('lodash.filter');
-var find = require('lodash.find');
-var sum = require('lodash.sum');
-var reduce = require('lodash.reduce');
-var map = require('lodash.map');
-var keys = require('lodash.keys');
-var gte = require('lodash.gte');
-var values = require('lodash.values');
-var camelCase = require('lodash.camelcase');
-var union = require('lodash.union');
-var endsWith = require('lodash.endswith');
+console.log(library[0])
+library[0].author.email = "email1.gmail.com";
+library[1].author.email = "email2.gmail.com";
+console.log(library[0])
 
+console.log(library[1].readers[0])
+library[1].readers.push({name: "person3", count : 550})
+console.log(library[1].readers[0])
 
-array=['b', 'b', 'a', 'a', 'c', 'b','d','a'];
-console.log(pull(array, 'a', 'c'))
-console.log(difference([3,5], [4,1,3]));
-console.log(take([4,7,8,1,2], 3));
+console.log(library[2]);
+library.push(
+    {
+        title: "SQL",
+        price: 400,
+        readers: [],
+        author: {
+            name: "Ramesh",
+            age: 80
+        }
+    }
+)
 
-var users = [
-    { 'Name': 'barney', 'age': 36 },
-    { 'Name': 'fred',   'age': 40 }
-  ];
-  console.log(filter(users, { 'age': 36 }));
-  console.log(find(users, { 'age': 11}));
-
-  console.log(sum([4, 2, 8, 6]));
-  console.log(reduce([1, 2], function(sum, n) {
-    return sum + n;
-  }, 0));
-
-  console.log(gte(17,10));
-  console.log(keys('hi'))
-  console.log(values('hi'));
-  console.log(camelCase('--lorem-Hwgug--'));
-  console.log(endsWith('abc', 'b', 2))
-  console.log(union([2], [1, 2,3]))
+console.log(library[2]);
+console.log(typeof(library[1].author.age));
